d29ee4afdfaa6473b09196439bd2dec0a18f0eda
unknown <isd6542@outlook.com>
2024-11-10 20:08:33 +1100

    Added results verification

--- handleExport.js
+++ handleExport.js
@@ -1,3 +1,14 @@
 // Get the data
+import results from "./proc_dta.ts";
+
+// Verify integrity of data
+if (typeof results.diagnosis != "string") {
+    throw new Error(
+        "[CRT] [ERR] [Dx] Invalid diagnosis. Expected diagnosis to be string, where diagnosis is" +
+        typeof results.diagnosis
+    );
+} else {
+    console.info("[Dx] Diagnosis received and is valid. It is: " + results.diagnosis);
+}
 
 // Pass off the data
