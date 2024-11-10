0ee6af370d5ea954db8eeadd1864bda6d59182bd
unknown <isd6542@outlook.com>
2024-11-10 20:31:51 +1100

    Made main file, made Results class

--- proc_dta.js
+++ proc_dta.js
@@ -1,31 +1,52 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //* TESTING TENSORFLOW
 // Define a model for linear regression. The script tag makes `tf` available as a global variable.
-var model = tf.sequential();
+const model = tf.sequential();
 model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
 model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+
 // Generate some synthetic data for training.
-var xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-var ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
 // Train the model using the data.
-model.fit(xs, ys, { epochs: 10 }).then(function () {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    model.predict(tf.tensor2d([5], [1, 1])).print();
-    // Open the browser devtools to see the output
+model.fit(xs, ys, { epochs: 10 }).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5], [1, 1])).print();
+  // Open the browser devtools to see the output
 });
 //* END TESTING TENSORFLOW
 // Model
+
 // Import user provided data
+
 // Process categorising the data
 // i.e. what type of cancer, etc.
+
 // Export
-var results = function () {
-    return "e";
-};
+//TODO: Move this into a new file
+class Results {
+    constructor(diagnosis) {
+        this.diagnosis = null;
+    }
+    destruct() {
+        delete this.diagnosis;
+        delete this;
+    }
+    diagnose(diagnosis) {
+        if (typeof diagnosis != "string") {
+            throw new Error(
+                "[CRT] [ERR] [Dx] Invalid diagnosis. Expected diagnosis to be string, where diagnosis is" +
+                typeof diagnosis
+            );
+        }
+        this.diagnosis = diagnosis;
+    }
+}
 // Send off data to remote server for further processing
 // Also to add data to further train AI
 // Server located @ 192.9.187.53
 //TODO: How to do this?
+
 // Pass off the data
-exports.default = results;
+export default Results;
