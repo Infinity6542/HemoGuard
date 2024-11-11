ba9ca5b848a1d2afd16c917d4cb3962b3434cf82
Infinity <isd6542@gmail.com>
2024-11-11 11:40:42 +1100

    Added better indents

--- proc_dta.js
+++ proc_dta.js
@@ -11,9 +11,9 @@ const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
 // Train the model using the data.
 model.fit(xs, ys, { epochs: 10 }).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5], [1, 1])).print();
-  // Open the browser devtools to see the output
+    // Use the model to do inference on a data point the model hasn't seen before:
+    model.predict(tf.tensor2d([5], [1, 1])).print();
+    // Open the browser devtools to see the output
 });
 //* END TESTING TENSORFLOW
 // Model
@@ -43,6 +43,7 @@ class Results {
         this.diagnosis = diagnosis;
     }
 }
+
 // Send off data to remote server for further processing
 // Also to add data to further train AI
 // Server located @ 192.9.187.53
