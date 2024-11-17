686771282b6a19e980bff5dfa2cddc51b7d884d6
Infinity <isd6542@gmail.com>
2024-11-18 10:25:03 +1100

    Merge diagnosis and append, change diagnosis to integer

--- results.js
+++ results.js
@@ -1,6 +1,6 @@
 class Results {
     constructor(diagnosis) {
-        this.diagnosis = null;
+        this.diagnosis = 0;
         this.items = [];
         this.count = 0;
     }
@@ -10,24 +10,29 @@ class Results {
         delete this.count;
         delete this;
     }
+    // diagnose(diagnosis) {
+    //     if (typeof diagnosis != "string") {
+    //         throw new Error(
+    //             "[CRT] [ERR] [Dx] Invalid diagnosis value type. Expected diagnosis to be string, where diagnosis is" +
+    //             typeof diagnosis
+    //         );
+    //     }
+    //     this.diagnosis = diagnosis;
+    // }
     diagnose(diagnosis) {
-        if (typeof diagnosis != "string") {
+        if (typeof diagnosis.name != "string") {
             throw new Error(
                 "[CRT] [ERR] [Dx] Invalid diagnosis value type. Expected diagnosis to be string, where diagnosis is" +
                 typeof diagnosis
             );
-        }
-        this.diagnosis = diagnosis;
-    }
-    append(item) {
-        if (typeof item.name != "string") {
-            throw new Error(
-                "[CRT] [ERR] [Dx] Invalid item value type. Expected item to be string, where item is" +
-                typeof item
-            );
-        } else if (item.category != 0) {
+        } else if (diagnosis.category != 0) {
             this.count++;
         }
-        this.items.push(item.name);
+
+        if (this.count != 0 && this.diagnosis == 0) {
+            this.diagnosis = -1;
+        }
+
+        this.diagnosis.push(diagnosis.name);
     }
 }
