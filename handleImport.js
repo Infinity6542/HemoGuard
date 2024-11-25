aa9ae3d4f0a7bb3ba15ec1901656d19ee7c4c28b
unknown <isd6542@outlook.com>
2024-11-26 07:47:40 +1100

    Import system

--- handleImport.js
+++ handleImport.js
@@ -1,21 +1,5 @@
-// Get the data
-let input = document.getElementById("input").files[0];
-
-// Check that data exists
-if (!input) {
-    alert("[CRT] [INP] [DTA] No data provided.");
-    return -1;
-} else if (typeof text !== "string") {
-    return false;
-} else {
-    try {
-        // Parse the data
-        let proc_input = JSON.parse(text);
-        return 0;
-    } catch (error) {
-        console.error("Error parsing data:", error);
-        return -1;
-    }
+function handleHoverOver(event) {
+    event.preventDefault();
 }
 
 // Pass off the data
