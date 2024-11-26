36ca26d900e4faea03b1807652d9d6aed5837842
unknown <isd6542@outlook.com>
2024-11-26 19:03:51 +1100

    Edited formatting

--- handleImport.js
+++ handleImport.js
@@ -9,8 +9,8 @@ console.log("State of reader is currently " + reader.readyState);
 // Called in DOM ondragover
 function handleHoverOver(event) {
 	event.preventDefault();
-    // Just to make sure there's nothing else
-    localStorage.clear();
+	// Just to make sure there's nothing else
+	localStorage.clear();
 }
 
 // Called in DOM ondrop
@@ -29,28 +29,30 @@ async function handleDrop(e) {
 		// reader.onload = () => {
 		console.log("State of reader is currently " + reader.readyState);
 		reader.readAsText(e.dataTransfer.files[0]);
-        // For whatever reason, onload just didn't work here
-        // (believe me, I tried)
-        // (An hour of my life I'll never get back)
-        // Also tried addEventListener
-        // So we're just going to sleep for 500ms and pray it's done then
-        // The next guy can increase the following counter:
-        // Time wasted on this: 1hr
+		// For whatever reason, onload just didn't work here
+		// (believe me, I tried)
+		// (An hour of my life I'll never get back)
+		// Also tried addEventListener
+		// So we're just going to sleep for 500ms and pray it's done then
+		// The next guy can increase the following counter:
+		// Time wasted on this: 1hr
 		await sleep(500);
 		console.log("State of reader is currently " + reader.readyState);
 		let raw = reader.result.toString();
-		let cells = raw.split('\n').map(function (el) { return el.split(/\s+/); });
-        let headings = cells.shift();
-        let obj = cells.map(function (el) {
-            var obj = {};
-            for (var i = 0, l = el.length; i < l; i++) {
-              obj[headings[i]] = isNaN(Number(el[i])) ? el[i] : +el[i];
-            }
-            return obj;
-          });
-          let data = JSON.stringify(obj);
-          // JSON.parse() when using it
-          localStorage.setItem("data", data);
+		let cells = raw.split("\n").map(function (el) {
+			return el.split(/\s+/);
+		});
+		let headings = cells.shift();
+		let obj = cells.map(function (el) {
+			var obj = {};
+			for (var i = 0, l = el.length; i < l; i++) {
+				obj[headings[i]] = isNaN(Number(el[i])) ? el[i] : +el[i];
+			}
+			return obj;
+		});
+		let data = JSON.stringify(obj);
+		// JSON.parse() when using it
+		localStorage.setItem("data", data);
 		// };
 	}
 }
